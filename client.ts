975c8a8fa0b6bc9ddd1a0dd3a54816aaa702bd5b
MustSave <dbstjdgk1541@gmail.com>
2023-09-11 19:45:49 +0900

    Chore: client에서 jwt를 사용하는 예제 업데이트

--- client.ts
+++ client.ts
@@ -1,6 +1,15 @@
 import { io } from "socket.io-client";
+import jwt from "jsonwebtoken";
 
-const socket = io("http://localhost:3000/", {path: "/ws"});
+const secret = process.env.SECRET || "secret";
+const token = jwt.sign({username: "user1", role: "user"}, secret);
+
+const socket = io("http://localhost:3000/", {
+    path: "/ws",
+    auth: {
+        token: token,
+    }
+});
 
 socket.on("connect", () => {
     console.log(`connect ${socket.id}`);
