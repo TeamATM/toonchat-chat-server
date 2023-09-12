6bc9289e29bc291d9ffd47d1a5b0006a6e5c2af2
MustSave <dbstjdgk1541@gmail.com>
2023-09-12 11:10:30 +0900

    Chore: 클라이언트 통신 예제 업데이트

--- client.ts
+++ client.ts
@@ -1,16 +1,29 @@
-import { io } from "socket.io-client";
+import { Socket, io } from "socket.io-client";
 import jwt from "jsonwebtoken";
+import { ClientToServerEvents, ServerToClientEvents } from "./types";
 
 const secret = process.env.SECRET || "secret";
-const token = jwt.sign({username: "user1", role: "user"}, secret);
+const token = jwt.sign({sub: "user1", role: "user"}, secret);
 
-const socket = io("http://localhost:3000/", {
+const socket:Socket<ServerToClientEvents, ClientToServerEvents> = io("http://localhost:3000/", {
     path: "/ws",
     auth: {
-        token: token,
+        // token: token,
     }
 });
 
 socket.on("connect", () => {
     console.log(`connect ${socket.id}`);
+
+    setTimeout(()=>{
+        socket.emit("publish", "hello")
+    }, 1000)
+
+    setInterval(()=> {
+        socket.emit("publish", "A");
+    }, 5000);
+
+    socket.on("subscribe", msg => {
+        console.log(msg);
+    })
 })
