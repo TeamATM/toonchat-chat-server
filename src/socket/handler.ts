e55355055ba5e7200832752525e29366fdbe73b3
MustSave <dbstjdgk1541@gmail.com>
2023-09-17 21:18:23 +0900

    Refactor: 함수 쪼개기

--- src/socket/handler.ts
+++ src/socket/handler.ts
@@ -8,33 +8,45 @@ import { generateRandomId } from "../utils";
 
 const maxMessageLength = Number(process.env.MAX_MESSAGE_LENGTH) || 100;
 
-async function handleOnPublishMessage(socket:TypeSocket, data:MessageFromClient) {
-    const { username } = socket.data;
+function checkCanRequest(userId:string, content:string) {
+    let errorMessage:string = "";
 
     // 글자수 제한
-    if (!data.content || data.content.length > maxMessageLength) {
-        socket.emit("error", { content: "Message is empty or too long" });
-        console.error(`Message length exceeded from user: ${username}, message: ${data.content}`);
-        return;
+    if (content || content.length > maxMessageLength) {
+        console.error(`Message length exceeded from user: ${userId}, message: ${content}`);
+        errorMessage = "Message is empty or too long";
+        return errorMessage;
     }
 
     // 한 계정당 동시 요청 가능 개수 제한
-    if (existMessageInProcess(username)) {
-        socket.emit("error", { content: "아직 처리중인 메시지가 있습니다. 이전 요청이 처리된 이후에 다시 시도하세요" });
-        console.error(`Too many requests from user: ${username}`);
+    if (existMessageInProcess(userId)) {
+        console.error(`Too many requests from user: ${userId}`);
+        errorMessage = "아직 처리중인 메시지가 있습니다. 이전 요청이 처리된 이후에 다시 시도하세요";
+        return errorMessage;
+    }
+
+    return errorMessage;
+}
+
+async function handleOnPublishMessage(socket:TypeSocket, data:MessageFromClient) {
+    const { username: userId } = socket.data;
+
+    const errorMessage = checkCanRequest(userId, data.content);
+    if (errorMessage) {
+        socket.emit("error", { content: errorMessage });
         return;
     }
 
     // 몽고디비 연결해서 메시지 저장 & 이전 대화내역 가져오기 => 메시지큐 전달
     Promise.all([
-        saveUserMessage(username, data.characterId, data.content),
-        getChatHistoryByLimit(username, data.characterId, 10),
+        saveUserMessage(userId, data.characterId, data.content),
+        getChatHistoryByLimit(userId, data.characterId, 10),
     ]).then((result) => {
         const [message, history] = result;
 
-        console.log(`Message from ${username}: ${message.content}`);
+        console.log(`Message from ${userId}: ${message.content}`);
         // ACK를 위한 pub
-        publish("amq.topic", username, buildMessage(message));
+        publish("amq.topic", userId, buildMessage(message));
         // 추론을 위한 pub
         publish("celery", "celery", buildMessage(message, history));
     }).catch(console.error);
