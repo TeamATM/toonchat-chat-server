94cec0ddfea8d537ed132aa05aed092f70e80983
MustSave <dbstjdgk1541@gmail.com>
2023-11-07 00:28:58 +0900

    Fix: fix mock ai example code

--- mock_endpoint/ai.ts
+++ mock_endpoint/ai.ts
@@ -1,9 +1,12 @@
+import "reflect-metadata";
+import Container from "typedi";
 import "../src/config/config";
+import { RabbitTemplate } from "../src/repository";
 import { logger } from "../src/config";
-import { subscribe, publish } from "../src/message_queue";
 import { MessageFromInferenceServer, MessageToInferenceServer } from "../src/types";
 
-subscribe("celery", "celery", { durable: true, autoDelete: false }, async (message) => {
+const rabbitTemplate = Container.get(RabbitTemplate);
+rabbitTemplate.subscribe("celery", "celery", { durable: true, autoDelete: false }, async (message) => {
     try {
         const tmp:MessageToInferenceServer = JSON.parse(message.content.toString("utf-8"));
         logger.trace(tmp);
@@ -15,7 +18,7 @@ subscribe("celery", "celery", { durable: true, autoDelete: false }, async (messa
             fromUser: false,
             content: `this is a message from botId: ${tmp.args[0].history.characterId}`,
         };
-        publish(
+        rabbitTemplate.publish(
             "amq.topic",
             data.userId,
             data,
